d6cf72511b26a87b7498b9c2ee7d389678a299f9
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2022-01-20 19:00:09 +0530

    Create 1.  Scatter_plot.py

---
+++ Matplotlib/1.  Scatter_plot.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+# Import dataset 
+midwest = pd.read_csv("https://raw.githubusercontent.com/selva86/datasets/master/midwest_filter.csv")
+
+# Prepare Data 
+# Create as many colors as there are unique midwest['category']
+categories = np.unique(midwest['category'])
+colors = [plt.cm.tab10(i/float(len(categories)-1)) for i in range(len(categories))]
+
+# Draw Plot for Each Category
+plt.figure(figsize=(16, 10), dpi= 80, facecolor='w', edgecolor='k')
+
+for i, category in enumerate(categories):
+    plt.scatter('area', 'poptotal', 
+                data=midwest.loc[midwest.category==category, :], 
+                s=20, c=colors[i], label=str(category))
+
+# Decorations
+plt.gca().set(xlim=(0.0, 0.1), ylim=(0, 90000),
+              xlabel='Area', ylabel='Population')
+
+plt.xticks(fontsize=12); plt.yticks(fontsize=12)
+plt.title("Scatterplot of Midwest Area vs Population", fontsize=22)
+plt.legend(fontsize=12)    
+plt.show()
